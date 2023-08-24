ce2a2c3a58e977cf09ce0e0fbfe8f7e7e94227ef
Adrian Alarcon <adrian.alarcon@invgate.com>
2023-08-24 17:32:44 -0600

    formateo

--- tiene3Dijitos.js
+++ tiene3Dijitos.js
@@ -1,12 +1,13 @@
 
-    // Si el número recibido tiene tres dígitos retornar true.
-    // Caso contrario, retornar false.
-    // Tu código:
-    function tieneTresDigitos(num) {
-    if (num>99 && num<1000){
-       console.log(true);}
-       else{
-          console.log(false);
-       }}
-       tieneTresDigitos(99);
- 
+// Si el número recibido tiene tres dígitos retornar true.
+// Caso contrario, retornar false.
+// Tu código:
+function tieneTresDigitos(num) {
+   if (num > 99 && num < 1000) {
+      console.log(true);
+   }
+   else {
+      console.log(false);
+   }
+}
+tieneTresDigitos(99);
